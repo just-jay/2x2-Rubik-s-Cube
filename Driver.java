8694f8e2f7a056cdf68e7a513aa00c04748f1956
Just_Jay <jakeelspas@gmail.com>
2017-01-02 22:01:04 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,20 @@
-//text Driver to build the cube and test turning
+
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cube c = new Cube();
-		
-		c.TurnLayerRightClockwise();
-		c.TurnLayerUpClockwise();
-		c.TurnLayerBackClockwise();
-		c.TurnLayerDownClockwise();
-		c.TurnLayerLeftClockwise();
-		c.TurnLayerFrontClockwise();
-		c.print();
-	}
 
+		double averageMoves = 0;
+		for(int i=0; i < 100000; i++){
+			String s = Cube.generateScramble();		
+			c.scramble(s);
+			//System.out.println(c.Solve());
+			averageMoves+= c.Solve();	
+		} 
+		System.out.println("Average Moves: " + averageMoves/100000);
+	}
 }
+
+//39.66538
+//39.70893
